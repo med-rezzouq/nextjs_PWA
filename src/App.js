0a0356e0286cd05fa94c4d46bec9afb627939cb0
rezzouq <mohammed.rezzouq@siway.ma>
2022-07-12 23:42:04 +0100

    ci(start with pwa):

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ function App() {
   const search = async (e) => {
     if (e.key == "Enter") {
       const data = await fetchWeather(query);
+      setWeather(data);
       console.log(data);
+      setQuery("");
     }
   };
   return (
@@ -23,6 +25,29 @@ function App() {
         onChange={(e) => setQuery(e.target.value)}
         onKeyPress={search}
       />
+
+      {weather.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span> {weather.name} </span>
+            <sup> {weather.sys.country}</sup>
+          </h2>
+
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup> &deg;C</sup>
+          </div>
+
+          <div className="info">
+            <img
+              className="city-icon"
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
+            />
+            <p>{weather.weather[0].description}</p>-
+          </div>
+        </div>
+      )}
     </div>
   );
 }
